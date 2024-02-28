c6c4f0dd867f1440f2bb641d6303fd8de0f4b22a
Emre Eker <emeke21@student.sdu.dk>
2024-02-28 12:06:29 +0100

    break commit

---
+++ src/routes/graphical/elementSelactor.svelte
@@ -0,0 +1,11 @@
+<script>
+    import ClassBox from "./classBox.svelte";
+</script>
+
+<div
+    class="bg-base-200 rounded-[15px] border-solid border-base-400 border-[5px] w-[263px] h-[428px] relative">
+    <div class="flex items-center justify-center pt-5">
+        <ClassBox />
+    </div>
+    
+</div>
