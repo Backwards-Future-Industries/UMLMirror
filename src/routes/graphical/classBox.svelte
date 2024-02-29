0ed3188ea7636ec1000a97ad74159cd3f1ad6cd6
Emre Eker <90039700+Wuldnarp@users.noreply.github.com>
2024-02-29 12:31:56 +0100

    Figma to svelte  (#1)
    * break commit
    
    * removed shit and made navbar
    
    * fixed classbox and dumb draggable

---
+++ src/routes/graphical/classBox.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Draggable from "./draggable.svelte";
+    let title = "Title";
+    let attributes = "Attribute";
+    let methods = "Methods";
+</script>
+
+<div>
+    <Draggable>
+        <div
+        contenteditable="true" bind:textContent={title} class="rounded-tl-[10px] rounded-tr-[10px] border-solid border-[#000000] border-2 flex items-center justify-center">
+        {title}
+        </div>
+        <div
+            contenteditable="true" bind:textContent={attributes} class="border-solid border-[#000000] border-r-2 border-b-2 border-l-2">
+            {attributes}
+        </div>
+        <div
+            contenteditable="true" bind:textContent={methods} class="rounded-br-[10px] rounded-bl-[10px] border-solid border-[#000000] border-r-2 border-b-2 border-l-2">
+            {methods}
+        </div>
+    </Draggable>
+</div>
---
+++ src/routes/navBar.svelte
@@ -0,0 +1,20 @@
+
+<div
+    class="bg-green-500 flex flex-row-reverse justify-end">
+  <div
+    class="bg-green-600 pl-10 pr-10 pt-5 pb-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl">
+    <a href="/dynamic">Dynamic</a>
+  </div>
+  <div
+    class="bg-green-700 pl-10 pr-10 pt-5 pb-5 -mr-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl">
+    <a href="/textual">Textual</a>
+  </div>
+  <div
+    class="bg-green-800 pl-10 pr-10 pt-5 pb-5 -mr-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl">
+    <a href="/graphical">Graphical</a>
+  </div>
+  <div
+    class="bg-green-900 pl-10 pr-10 pt-5 pb-5 -mr-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl">
+    <a href="/">UMLPlanter</a>
+  </div> 
+</div>
