42ef9e08af97bdaaabf2111ab797f872bc828ec3
Emre Eker <emeke21@student.sdu.dk>
2024-02-29 12:28:53 +0100

    fixed classbox and dumb draggable

--- src/routes/graphical/classBox.svelte
+++ src/routes/graphical/classBox.svelte
@@ -1,27 +1,23 @@
-<div class="w-[183px] h-[185px] relative">
-    <div
-        class="rounded-br-[10px] rounded-bl-[10px] border-solid border-[#000000] border-r-2 border-b-2 border-l-2 absolute right-[0%] left-[0%] w-[100%] bottom-[0%] top-[61.08%] h-[38.92%]">
-    </div>
-    <div
-        class="border-solid border-[#000000] border-r-2 border-b-2 border-l-2 absolute right-[0%] left-[0%] w-[100%] bottom-[38.92%] top-[22.16%] h-[38.92%]">
-    </div>
-    <div
-        class="rounded-tl-[10px] rounded-tr-[10px] border-solid border-[#000000] border-2 absolute right-[0%] left-[0%] w-[100%] bottom-[77.84%] top-[0%] h-[22.16%]">
-    </div>
-    <div
-        class="text-[#000000] text-left font-['Inter-Regular',_sans-serif] text-base font-normal absolute right-[18.58%] left-[7.1%] w-[74.32%] bottom-[13.51%] top-[65.95%] h-[20.54%]">
-        - method(): void
-        <br />
-        + method2(): void
-    </div>
-    <div
-        class="text-[#000000] text-left font-['Inter-Regular',_sans-serif] text-base font-normal absolute right-[43.72%] left-[7.1%] w-[49.18%] bottom-[53.51%] top-[25.95%] h-[20.54%]">
-        - attribute
-        <br />
-        + attribute2
-    </div>
-    <div
-        class="text-[#000000] text-left font-['Inter-Regular',_sans-serif] text-2xl font-normal absolute right-[33.88%] left-[33.88%] w-[32.24%] bottom-[81.08%] top-[3.24%] h-[15.68%]">
-        Tittle
-    </div>
+<script>
+    import Draggable from "./draggable.svelte";
+    let title = "Title";
+    let attributes = "Attribute";
+    let methods = "Methods";
+</script>
+
+<div>
+    <Draggable>
+        <div
+        contenteditable="true" bind:textContent={title} class="rounded-tl-[10px] rounded-tr-[10px] border-solid border-[#000000] border-2 flex items-center justify-center">
+        {title}
+        </div>
+        <div
+            contenteditable="true" bind:textContent={attributes} class="border-solid border-[#000000] border-r-2 border-b-2 border-l-2">
+            {attributes}
+        </div>
+        <div
+            contenteditable="true" bind:textContent={methods} class="rounded-br-[10px] rounded-bl-[10px] border-solid border-[#000000] border-r-2 border-b-2 border-l-2">
+            {methods}
+        </div>
+    </Draggable>
 </div>
