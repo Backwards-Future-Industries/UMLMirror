2e31f277da348198de90c71f185b350908f5179e
Emre Eker <emeke21@student.sdu.dk>
2024-03-12 21:35:39 +0100

    trying to move a svg part1

--- src/lib/components/classBox.svelte
+++ src/lib/components/classBox.svelte
@@ -1,36 +1,32 @@
 <script>
-    import Draggable from "./draggable.svelte";
+    import { SVG, extend as SVGextend, Element as SVGElement } from '@svgdotjs/svg.js'
+    import '@svgdotjs/svg.draggable.js'
     let title = "Title";
     let attributes = "Attribute";
     let methods = "Methods";
-    let triangleWidth = 100;
-    let triangleHeight = 100;
+    let width = 100;
+    let height = 100;
 
-    function titlehandleclick() {
-        title = prompt("Enter the title of the class");
-    }
-    function attributeshandleclick() {
-        attributes = prompt("Enter the attributes of the class");
-    }
-    function methodshandleclick() {
-        methods = prompt("Enter the methods of the class");
-    }
+
+    var draw = SVG().addTo('body')
+    var classBox = draw.addClass('classbox')
+    classBox.draggable()
     
 </script>
 
-<Draggable>
-    <svg  width={triangleWidth} height={triangleHeight}>
-        <rect x="0" y="0" width={triangleWidth} height={triangleHeight} fill="none" stroke="black" stroke-width="2" rx="10" ry="10" />
-        <g on:click={titlehandleclick}>
-            <text x="50" y="20" text-anchor="middle" font-size="16" font-weight="bold">{title}</text>
-        </g>
-        <line x1="0" y1="30" x2="100" y2="30" stroke="black" stroke-width="2" />
-        <g on:click={attributeshandleclick}>
-            <text x="50" y="50" text-anchor="middle" font-size="16" font-weight="bold">{attributes}</text>
-        </g>
-        <line x1="0" y1="70" x2="100" y2="70" stroke="black" stroke-width="2" />
-        <g on:click={methodshandleclick}>
-            <text x="50" y="90" text-anchor="middle" font-size="16" font-weight="bold">{methods}</text>
-        </g>
-    </svg>
-</Draggable>
+<svg id="classbox">
+    <rect x="0" y="0" width={width} height={height} fill="none" stroke="black" stroke-width="2" rx="10" ry="10" />
+    <g>
+        <text x="50" y="20" text-anchor="middle" font-size="16" font-weight="bold">{title}</text>
+    </g>
+    <line x1="0" y1="30" x2="100" y2="30" stroke="black" stroke-width="2" />
+    <g>
+        <text x="50" y="50" text-anchor="middle" font-size="16" font-weight="bold">{attributes}</text>
+    </g>
+    <line x1="0" y1="70" x2="100" y2="70" stroke="black" stroke-width="2" />
+    <g>
+        <text x="50" y="90" text-anchor="middle" font-size="16" font-weight="bold">{methods}</text>
+    </g>
+</svg>
+
+
---
+++ src/lib/components/classButton.svelte
@@ -0,0 +1,3 @@
+<button on:click class=" bg-base-400 hover:bg-base-600 text-white font-bold py-2 px-4 rounded m-5">
+    Class
+  </button>
