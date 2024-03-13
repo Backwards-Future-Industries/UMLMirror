21f5910cd569ddf6955e0041279007cc947f8bec
Emre Eker <emeke21@student.sdu.dk>
2024-03-13 12:28:26 +0100

    movable svg component that is satisfying

--- src/lib/components/classBox.svelte
+++ src/lib/components/classBox.svelte
@@ -1,32 +1,54 @@
 <script>
-    import { SVG, extend as SVGextend, Element as SVGElement } from '@svgdotjs/svg.js'
-    import '@svgdotjs/svg.draggable.js'
     let title = "Title";
     let attributes = "Attribute";
     let methods = "Methods";
     let width = 100;
     let height = 100;
-
-
-    var draw = SVG().addTo('body')
-    var classBox = draw.addClass('classbox')
-    classBox.draggable()
     
+    let x = 0;
+    let y = 0;
+
+    let moving = false;
+	
+	function onMouseDown() {
+		moving = true;
+	}
+	
+	function onMouseMove(e) {
+		if (moving) {
+			x += e.movementX;
+			y += e.movementY;
+		}
+	}
+	
+	function onMouseUp() {
+		moving = false;
+	}
+
 </script>
 
-<svg id="classbox">
-    <rect x="0" y="0" width={width} height={height} fill="none" stroke="black" stroke-width="2" rx="10" ry="10" />
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<svg on:mousedown={onMouseDown} class="draggable">
+    <rect x={x} y={y} width={width} height={height} fill="none" stroke="black" stroke-width="2" rx="10" ry="10" />
     <g>
-        <text x="50" y="20" text-anchor="middle" font-size="16" font-weight="bold">{title}</text>
+        <text x={x+50} y={y+20} text-anchor="middle" font-size="16" font-weight="bold">{title}</text>
     </g>
-    <line x1="0" y1="30" x2="100" y2="30" stroke="black" stroke-width="2" />
+    <line x1={x} y1={y+30} x2={x+100} y2={y+30} stroke="black" stroke-width="2" />
     <g>
-        <text x="50" y="50" text-anchor="middle" font-size="16" font-weight="bold">{attributes}</text>
+        <text x={x+50} y={y+50} text-anchor="middle" font-size="16" font-weight="bold">{attributes}</text>
     </g>
-    <line x1="0" y1="70" x2="100" y2="70" stroke="black" stroke-width="2" />
+    <line x1={x} y1={y+70} x2={x+100} y2={y+70} stroke="black" stroke-width="2" />
     <g>
-        <text x="50" y="90" text-anchor="middle" font-size="16" font-weight="bold">{methods}</text>
+        <text x={x+50} y={y+90} text-anchor="middle" font-size="16" font-weight="bold">{methods}</text>
     </g>
 </svg>
 
+<style>
+	.draggable {
+		user-select: none;
+		cursor: move;
+    }
+</style>
 
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
--- src/routes/graphical/+page.svelte
+++ src/routes/graphical/+page.svelte
@@ -1,27 +1,16 @@
 <script>
   import ClassButton from "$lib/components/classButton.svelte";
+  import ClassBox from "$lib/components/classBox.svelte";
 
-  let width = 1000;
+  let width = 1000
   let height = 1000;
 
 
   function handleClick(){
-    document.getElementById('diagram').innerHTML += `
-    <svg class="draggable">
-      <rect x="0" y="0" width="100" height="100" fill="none" stroke="black" stroke-width="2" rx="10" ry="10" />
-      <g>
-        <text x="50" y="20" text-anchor="middle" font-size="16" font-weight="bold">Title</text>
-      </g>
-      <line x1="0" y1="30" x2="100" y2="30" stroke="black" stroke-width="2" />
-      <g>
-        <text x="50" y="50" text-anchor="middle" font-size="16" font-weight="bold">Attribute</text>
-      </g>
-      <line x1="0" y1="70" x2="100" y2="70" stroke="black" stroke-width="2" />
-      <g>
-        <text x="50" y="90" text-anchor="middle" font-size="16" font-weight="bold">Methods</text>
-      </g>
-    </svg>
-    `;
+    let newClass = new ClassBox({
+      target: document.getElementById('diagram'),
+    });
+    
   }
 </script>
 
@@ -30,6 +19,5 @@
     <ClassButton on:click={handleClick} />
   </div>
   <svg id="diagram" class="flex" width={width} height={height}>
-
   </svg>
 </div>
