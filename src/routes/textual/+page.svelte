26f8186f52952db1e30879da5e5b2c4c87a452ca
OFHansen <ohans21@student.sdu.dk>
2024-03-14 10:09:17 +0100

    Basic textarea implemented

---
+++ src/routes/textual/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "../../app.css";
+</script>
+
+<div class="flex flex-row relative top-0 left-0 h-screen">
+    <textarea class="w-textarea-width resize-none h-full bg-base-200 pl-3 pt-2 pr-3"/>
+</div>
