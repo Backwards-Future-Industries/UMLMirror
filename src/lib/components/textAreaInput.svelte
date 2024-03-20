70bcd7846078d37fa6a3f92c334b41ae75dddb65
OFHansen <90039820+OFHansen@users.noreply.github.com>
2024-03-20 15:29:04 +0100

    #12 basic textual implementation (#14)
    Text area no no parsing, yet :)
    
    * Basic textarea implemented
    
    * Moved textarea to component, undid changes to tailwind.config
    
    * Json meta model (#13)
    
    * meta model proposal
    
    * meta model proposal fix
    
    * add tsconfig.json
    
    * strict null and no implicit
    
    * Changed to ts in component
    
    * empty string for text area
    
    ---------

---
+++ src/lib/components/textAreaInput.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    
+    let textArea: string =  '';
+
+</script>
+
+
+
+<div class="flex flex-col">
+    <textarea id="diagramtext" class="resize-none w-96 bg-base-200 pl-3 pt-2 pr-3 font-mono flex-grow h-screen" spellcheck="false" bind:value={textArea}/>
+    <button on:click class=" bg-base-400 hover:bg-base-600 text-white font-bold py-2 px-4 rounded">
+        create diagram
+    </button>
+</div>
---
+++ src/routes/textual/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import TextAreaInput from "../../lib/components/textAreaInput.svelte";
+    import "../../app.css";
+
+    function testButton() {
+        alert("test");
+    }
+
+</script>
+
+<div class="flex flex-row relative top-0 left-0">
+    <TextAreaInput on:click={testButton}/>
+    <div>test</div>
+</div>
