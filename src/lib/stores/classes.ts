b5d0d09c90bf526b05a140a3ec16eed7f4627349
Emre Eker <emeke21@student.sdu.dk>
2024-03-20 19:05:23 +0100

    store as dictionary and ekstra methods added

--- src/lib/stores/classes.ts
+++ src/lib/stores/classes.ts
@@ -1,10 +1,14 @@
 import { writable, type Updater } from 'svelte/store';
 
-export function createClasses(onchange?: (value: classStoreObject[]) => void){
+interface Dictionary {
+    [key: string]: classStoreObject;
+}
+
+export function createClasses(onchange?: (value: Dictionary) => void){
     
-    const classes = writable<classStoreObject[]>([]);
-    
-    function update(updater: Updater<classStoreObject[]>){
+    const classes = writable<Dictionary>({});
+        
+    function update(updater: Updater<Dictionary>){
         classes.update((current) => {
             const newvalue = updater(current);
             if(onchange){
@@ -14,8 +18,31 @@ export function createClasses(onchange?: (value: classStoreObject[]) => void){
         });
     }
 
+    function add(key: string, value: classStoreObject): void {
+        update(current => ({ ...current, [key]: value }));
+    }
+
+    function remove(key: string): void {
+        update(current => {
+            const newState = { ...current };
+            delete newState[key];
+            return newState;
+        });
+    }
+
+    function get(key: string): classStoreObject | undefined {
+        let itemValue: classStoreObject | undefined;
+        classes.subscribe($classes => {
+            itemValue = $classes[key];
+        })();
+        return itemValue;
+    }
+
     return {
         ...classes,
-        update
+        update,
+        add,
+        remove,
+        get
     }
 }
