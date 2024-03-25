e6eb22f61d3da018dec4f5efd45e35138daaf372
Emre Eker <emeke21@student.sdu.dk>
2024-03-25 12:53:58 +0100

    new approach

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 #https://gist.github.com/aradalvand/04b2cad14b00e5ffe8ec96a3afbb34fb
 FROM node:18 AS builder
+LABEL org.opencontainers.image.source="https://github.com/<your-github-username>/<your-repo-name>"
 WORKDIR /app
 COPY package*.json .
 RUN npm ci
--- ci/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
-[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username_lower}/"
-docker build -t "${DockerRepo}umlmirror-app:latest" -t "${DockerRepo}umlmirror-app:1.0-$Tag" .
