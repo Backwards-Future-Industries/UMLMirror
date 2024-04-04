b8fb31fb5d89f391551538f1ea6d39bba4b50ec2
OFHansen <ohans21@student.sdu.dk>
2024-04-04 07:50:31 +0200

    class format decided

--- src/routes/textual/+page.svelte
+++ src/routes/textual/+page.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
     import TextAreaInput from "$lib/components/textAreaInput.svelte";
 
-    let diagramtext: string = "";
+    let diagramText: string =  "class: Person {" +
+    "\nat:"+
+    "\n\"- name: String\"" +
+    "\n\"- age: int\"" +
+    "\nme:"+
+    "\n\"+ getName(): String\"" +
+    "\n\"+ getAge(): String\"" +
+    "\n}";
 
-    function testButton() {
-        console.log(diagramtext);
+    function createDiagram() {
+        console.log(diagramText);
     }
 
 </script>
 
 <div class="flex flex-row relative top-0 left-0">
-    <TextAreaInput bind:textArea={diagramtext} on:click={testButton}/>
+    <TextAreaInput bind:textArea={diagramText} on:click={createDiagram}/>
     <div>test</div>
 </div>
