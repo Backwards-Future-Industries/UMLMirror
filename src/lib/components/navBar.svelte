f39cea4b8927ef32deaae248563f8c9d96b808b5
OFHansen <ohans21@student.sdu.dk>
2024-04-16 11:48:33 +0200

    Fixed navbar rounding

--- src/lib/components/navBar.svelte
+++ src/lib/components/navBar.svelte
@@ -2,19 +2,19 @@
 <div
     class="bg-green-500 flex flex-row-reverse justify-end">
   <div
-    class="bg-green-600 pl-10 pr-10 pt-5 pb-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl">
+    class="bg-green-600 pl-10 pr-10 pt-5 pb-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl z-10">
     <a href="/dynamic">Dynamic</a>
   </div>
   <div
-    class="bg-green-700 pl-10 pr-10 pt-5 pb-5 -mr-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl">
+    class="bg-green-700 pl-10 pr-10 pt-5 pb-5 -mr-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl z-20">
     <a href="/textual">Textual</a>
   </div>
   <div
-    class="bg-green-800 pl-10 pr-10 pt-5 pb-5 -mr-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl">
+    class="bg-green-800 pl-10 pr-10 pt-5 pb-5 -mr-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl z-30">
     <a href="/graphical">Graphical</a>
   </div>
   <div
-    class="bg-green-900 pl-10 pr-10 pt-5 pb-5 -mr-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl">
+    class="bg-green-900 pl-10 pr-10 pt-5 pb-5 -mr-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl z-40">
     <a href="/">UMLPlanter</a>
   </div> 
 </div>
