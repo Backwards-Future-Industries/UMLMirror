ace2b4ba27fde39138ee384459fc598e9a225481
OFHansen <ohans21@student.sdu.dk>
2024-04-16 19:28:11 +0200

    Outcommented Dynamic again

--- src/lib/components/navBar.svelte
+++ src/lib/components/navBar.svelte
@@ -1,7 +1,7 @@
 
 <div
     class="bg-green-500 flex flex-row-reverse justify-end">
-  <div
+  <!-- div
     class="bg-green-600 pl-10 pr-10 pt-5 pb-5 rounded-tr-[25px] rounded-br-[25px] text-white flex items-center justify-center font-mono text-2xl z-10">
     <a href="/dynamic">Dynamic</a>
     </div> -->
