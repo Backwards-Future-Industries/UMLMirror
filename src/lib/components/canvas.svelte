1c58d286566ec7ecd5272eb5648bfb3c9455897a
Emre Eker <emeke21@student.sdu.dk>
2024-05-01 00:00:22 +0200

    canvas autoscale with diagram

--- src/lib/components/canvas.svelte
+++ src/lib/components/canvas.svelte
@@ -4,8 +4,37 @@
     import { classes } from "$lib/stores/classes";
     import { associations } from "$lib/stores/associations";
 
-    let width: number = 1000
-    let height: number = 1000;
+    let width: number = furthestX();
+    let height: number = furthestY();
+
+    function furthestX(): number{
+        let furthest: number = 1000
+
+        let allClasses = Object.values($classes);
+        allClasses.forEach((value)=>{
+            if(furthest < value.getX() + value.getWidth()){
+                furthest = value.getX() + value.getWidth()
+            }
+        })
+        return furthest + 50
+    }
+    
+    function furthestY(): number{
+        let furthest: number = 1000
+
+        let allClasses = Object.values($classes);
+        allClasses.forEach((value)=>{
+            if(furthest < value.getY() + value.getHeight()){
+                furthest = value.getY() + value.getHeight()
+            }
+        })
+        return furthest + 50
+    }
+
+    classes.subscribe(()=>{
+        width = furthestX()
+        height = furthestY()
+    })
 
 </script>
 
