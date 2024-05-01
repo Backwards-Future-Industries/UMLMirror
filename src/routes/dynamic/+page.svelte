35e547e15e6548ff51914a93c5537617835d0718
OFHansen <ohans21@student.sdu.dk>
2024-05-01 11:36:44 +0200

    Updated dynamic for new widgets

--- src/routes/dynamic/+page.svelte
+++ src/routes/dynamic/+page.svelte
@@ -1,47 +1,13 @@
 <script>
-    import TextAreaInput from "$lib/components/textAreaInput.svelte";
-    import Canvas from "$lib/components/canvas.svelte";
-    import Button from "$lib/components/button.svelte";
-    import { handlePrettify } from "$lib/logic/graphvizLogic";
-    import { classes } from "$lib/stores/classes";
-    import { associations } from "$lib/stores/associations";
-    import { incrementer } from "$lib/stores/incrementer";
-    import { focus } from '$lib/stores/focus'
-    import { xClass } from '$lib/objects/xClass';
-
-    function handleClass(){
-        incrementer.increment();
-        let newClass = new xClass(incrementer.getString());
-        classes.add(incrementer.getString(), newClass);
-            
-        console.log(classes.getAll());
-    }
-
-    function deleteClass(){
-        classes.remove($focus);
-        associations.deleteFromKey($focus);
-    }
+  import TextAreaInput from "$lib/components/higherLevel/textAreaInput.svelte";
+  import Canvas from "$lib/components/higherLevel/canvas.svelte";
+  import ToolBar from "$lib/components/higherLevel/toolBar.svelte";
+  import { handlePrettify } from "$lib/logic/graphvizLogic";
 
 </script>
 
-
-
 <div class="flex flex-row relative top-0 left-0">
-    <TextAreaInput on:updateDraw={handlePrettify}/>
-    <Canvas/>
-    <div class="bg-base-200 rounded-[15px] border-solid border-base-400 border-[5px] w-[263px] h-[428px] relative flex flex-col items-center justify-start">
-        <Button name="Class" on:click={handleClass}/>
-        <Button name="Prettify" on:click={handlePrettify}/>
-        {#if $focus != '0'}
-          <p class="text-center">Focus is on: {classes.get($focus).name}</p>
-        {:else}
-          <p class="text-center">There is no class in focus</p>
-        {/if}
-        {#if $focus != '0'}
-          <Button name='Delete {classes.get($focus).name}' on:click={deleteClass}/>
-        {:else}
-          <p class="text-center">Press a class name to focus</p>
-        {/if}
-    
-      </div>
+  <TextAreaInput on:updateDraw={handlePrettify}/>
+  <Canvas/>
+  <ToolBar/>
 </div>
